01a9456856789decc6d916bdc1b896fc61798a85
heeger <fheeger@mi.fu-berlin.de>
2017-07-10 15:52:21 +0200

    redid reference selection from isolate OTUs; added error rate measurment from isolates

--- mapping.snakemake.py
+++ mapping.snakemake.py
@@ -26,20 +26,38 @@ samples = {"Lib4-0018": "mock",
            "Lib5-0027": "EV2",
            "Lib5-0095": "DT2",
            "Lib6-0009": "UM2",
-           "Lib6-0027": "IF1",
+           #"Lib6-0027": "IF1",
            "Lib6-0095": "CL2",
            "Lib7-0009": "SC2",
            "Lib7-0027": "LS2",
            "Lib7-0056": "PB1",
            "Lib7-0095": "Psp1",
            "Lib8-0009": "MR2",
-           "Lib8-0027": "IF2",
+#           "Lib8-0027": "IF2",
            "Lib8-0056": "PB2",
            "Lib8-0095": "Psp2",
            }
 
-ref = ["LSU", "SSU", "ITS"]
+isolates = {"CA": ["Lib1-0009", "Lib5-0009"],
+            "SC": ["Lib3-0009", "Lib7-0009"],
+            "CL": ["Lib1-0095", "Lib6-0095"],
+            "EV": ["Lib4-0027", "Lib5-0027"],
+            "PB": ["Lib7-0056", "Lib8-0056"],
+            "CHY1": ["Lib0-0009"],
+            "TR": ["Lib0-0056"], 
+            "PC": ["Lib1-0027", "Lib2-0027"],
+            "Csp": ["Lib1-0056", "Lib3-0056"],
+            "Psp": ["Lib7-0095", "Lib8-0095"],
+            "CR": ["Lib0-0075"],
+            "ME": ["Lib2-0056", "Lib4-0056"],
+            "UM": ["Lib2-0009", "Lib2-0009"],
+            "LS": ["Lib3-0027", "Lib7-0027"],
+            "DT": ["Lib3-0095", "Lib5-0095"],
+            #"IF": ["Lib6-0027", "Lib8-0027"],
+            "MR": ["Lib4-0009", "Lib8-0009"]
+            }
 
+ref = ["LSU", "SSU", "ITS"]
 
 rule mapping:
     input: reads="primers/{sample}_primer.fasta", ref="../PacBioMetabarcoding2/references/all_{ref}.fasta"
@@ -281,14 +299,49 @@ rule compareCluCls:
         
 
 rule removeChimeraRef:
-    input: seqs="primers/{sample}_primer.fasta", ref="isolateSeqs.fasta"
+    input: seqs="primers/{sample}_primer.fasta", ref="fullRef/isolateSeqs.fasta"
     output: fasta="chimeraRef/{sample}.nochimera.fasta", tsv="chimeraRef/{sample}.chimeraReport.tsv"
     log: "logs/{sample}_refChimera.log"
     shell:
         "%(vsearch)s --uchime_ref {input.seqs} --db {input.ref} --nonchimeras {output.fasta} --uchimeout {output.tsv} &> {log}" % config
 
+def fullRefInput(wildcards):
+    return ["consensus/%s_consensus.fasta" % s for s in isolates[wildcards.spec]]
+
+rule getFullRef:
+    input: fullRefInput
+    output: "fullRef/{spec}.fasta"
+    params: minSize=10
+    log: "logs/fullRef_{spec}.log"
+    run:
+        seq=[]
+        for r in range(len(input)):
+            seqList = []
+            for rec in SeqIO.parse(open(input[r]), "fasta"):
+                size = int(rec.id.strip(";").rsplit(";", 1)[-1].split("=")[1])
+                if size >= params.minSize:
+                    seqList.append(rec)
+            assert len(seqList) == 1
+            seq.append(seqList[0])
+        if len(input) > 1:
+            with open(log[0], "w") as logFile:
+                try:
+                    assert seq[0].seq == seq[1].seq
+                except AssertionError:
+                    logFile.write("Different sequences from replicates:\n\n")
+                    logFile.write(seq[0].format("fasta"))
+                    logFile.write(seq[1].format("fasta"))
+        seq[0].id = "isolate_%s" % wildcards.spec
+        open(output[0], "w").write(seq[0].format("fasta"))
+
+rule collectFullRef:
+    input: expand("fullRef/{spec}.fasta", spec=isolates.keys())
+    output: "fullRef/isolateSeqs.fasta"
+    shell:
+        "cat {input} > {output}"
+
 rule fullMapping:
-    input: reads="chimeraRef/{sample}.nochimera.fasta", ref="isolateSeqs.fasta"
+    input: reads="chimeraRef/{sample}.nochimera.fasta", ref="fullRef/isolateSeqs.fasta"
     output: m5="mapping/fullMapping/{sample}_vs_isolates.m5"
     threads: 6
     shell: 
@@ -306,8 +359,8 @@ rule getFullCls:
             if qcov < 0.9:
                 continue
             ident = float(numMatch)/(int(qEnd)-int(qStart))
-            spec = tName.split("_")[2]
-            mData = (ident, float(score))
+            spec = tName.split("_")[1]
+            mData = (str(ident), score, str(int(qEnd)-int(qStart)), numMismatch, numIns, numDel)
             rId = qName.rsplit("/", 1)[0]
             try:
                 qData = data[rId]
@@ -320,16 +373,64 @@ rule getFullCls:
         with open(output.matches, "w") as out:
             for rId, entryDict in data.items():
                 for spec, entry in entryDict.items():
-                    out.write("%s\t%s\t%s\t%s\t%i\n" % (rId, spec, entry[0], entry[1], len(entryDict)))
+                    out.write("%s\t%s\t%s\n" % (rId, spec, "\t".join(entry)))
         with open(output.assign, "w") as out:
             for line in open(input.chimera):
                 arr = line.strip().split("\t")
                 rId = arr[1]
                 if arr[-1] != "N":
-                    out.write("%s\tCHIMERA\n" % rId)
+                    out.write("%s\tCHIMERA\tNA\tNA\tNA\tNA\n" % rId)
                 elif rId in data:
                     matchDict = data[rId]
-                    best = sorted(matchDict.items(), key=lambda x: x[1][1])[0]
-                    out.write("%s\t%s\n" % (rId, best[0]))
+                    best = sorted(matchDict.items(), key=lambda x: int(x[1][1]))[0]
+                    out.write("%s\t%s\t%s\n" % (rId, best[0], "\t".join(best[1][2:6])))
                 else:
-                    out.write("%s\tUNKNOWN\n" % rId)
+                    out.write("%s\tUNKNOWN\tNA\tNA\tNA\tNA\n" % rId)
+
+rule collectFullCls:
+    input: expand("mapping/assignment/{sample}_assignments.tsv", sample=samples.keys())
+    output: "mapping/all_assignments.tsv"
+    run:
+        with open(output[0], "w") as out:
+            for inputFile in input:
+                sample = inputFile.rsplit("/", 1)[-1].split("_", 1)[0]
+                for line in open(inputFile):
+                    out.write("%s\t%s\t%s" % (sample, samples[sample], line))
+
+rule plotErrorRate:
+    input: "mapping/all_assignments.tsv"
+    output: "mapping/errorRates.svg"
+    run:
+        R("""
+        library(ggplot2)
+        library(reshape2)
+
+        d=read.table("{input}")
+
+        colnames(d) = c("sampleId", "sampleName", "readID", "cls", "alnLen", "sub", "ins", "del")
+        d$pSub = d$sub/d$alnLen
+        d$pIns = d$ins/d$alnLen
+        d$pDel = d$del/d$alnLen
+
+        e=subset(d, sampleName!="mock" & cls!="CHIMERA" & cls!="UNKNOWN")
+        p=melt(e, id.vars=c("sampleId", "sampleName", "readID", "cls"), measure.vars=c("pSub", "pIns", "pDel"))
+        ggplot(p) + geom_boxplot(aes(x=sampleName, y=value, fill=variable)) + labs(x="isolate sample", y="error rate", fill="error type") + scale_fill_brewer(labels=c("substitutions", "insertions", "deletions"), type="div") + theme_bw()
+        ggsave("{output}", width=16, height=10)
+        """)
+
+rule plotAssignment:
+    input: "mapping/all_assignments.tsv"
+    output: "mapping/assignments.svg"
+    run:
+        R("""
+        library(ggplot2)
+        
+        d=read.table("{input}")
+        colnames(d) = c("sampleId", "sampleName", "readID", "cls", "alnLen", "sub", "ins", "del")
+        d$cls=factor(d$cls, levels=c("UNKNOWN", "CHIMERA", "MR", "LS", "ME", "PB", "Psp", "CL", "SC", "DT", "CR", "TR", "PC", "Csp", "CHY1", "UM", "EV", "CA"))
+        
+        mycols = adjustcolor(rainbow(16), red.f = 0.7, blue.f = 0.7, green.f = 0.7)
+        mycols = c("grey", "black", mycols)
+        ggplot(d) + geom_bar(aes(x=sampleName, fill=cls)) +scale_fill_manual(values=mycols) + labs(x="sample", y="read number", fill="classification") + theme_bw()
+        ggsave("{output}", width=16, height=10)
+        """)

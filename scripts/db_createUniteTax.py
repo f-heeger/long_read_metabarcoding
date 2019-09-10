67267f67cc75ffb5ca1899de48fe900676272753
Felix Heeger <fheeger@mi.fu-berlin.de>
2019-09-10 16:56:49 +0200

    fixed bug in creting unite index

--- scripts/db_createUniteTax.py
+++ scripts/db_createUniteTax.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 
 with open(snakemake.output.tax, "w") as tOut, open(snakemake.output.fasta, "w") as fOut:
-    for rec in SeqIO.parse(snakemake.open(input[0]), "fasta"):
+    for rec in SeqIO.parse(open(snakemake.input[0]), "fasta"):
         arr = rec.id.split("|")
         sh = arr[2]
         tax = arr[-1]

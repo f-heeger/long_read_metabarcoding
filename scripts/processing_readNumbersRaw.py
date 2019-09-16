397aef7377e78781f8ad088ce2b25ccb9b19cbeb
Felix Heeger <fheeger@mi.fu-berlin.de>
2019-09-16 21:52:13 +0200

    fixed bug in raw read count

--- scripts/processing_readNumbersRaw.py
+++ scripts/processing_readNumbersRaw.py
@@ -1,10 +1,12 @@
+import gzip
+
 from Bio import SeqIO
 
 with open(snakemake.output[0], "w") as out:
     #raw
     for rawFileName in snakemake.input:
         i=0
-        with open(rawFileName) as rawFile:
+        with gzip.open(rawFileName, "rt") as rawFile:
             iter = SeqIO.parse(rawFile, "fastq")
             while True:
                 try:
@@ -13,4 +15,4 @@ with open(snakemake.output[0], "w") as out:
                     break
                 i+=1
             sample = rawFileName.split("/")[-1].split(".")[0]
-            out.write("raw\t%s\t%i\n" % (file2sample[rawFileName], i))
+            out.write("raw\t%s\t%i\n" % (sample, i))

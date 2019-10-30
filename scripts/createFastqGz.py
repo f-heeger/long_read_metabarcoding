8841fff997247f72ba107afe21de3e0cb7e307b1
heeger <fheeger@mi.fu-berlin.de>
2019-10-30 11:08:10 +0100

    createFastqGz script: fixed errors in handling of snakemake objects

--- scripts/createFastqGz.py
+++ scripts/createFastqGz.py
@@ -1,8 +1,8 @@
 import subprocess
 
-sraId = snakemkake.config["samples"][wilcards.sample]["sraId"]
+sraId = snakemake.config["samples"][snakemake.wildcards.sample]["sraId"]
 
 if len(sraId) == 0:
-    subprocess.run(["bam2fastq", "-o", "fastq/%s" % wildcards.sample, snakemake.input[0]])
+    subprocess.run(["bam2fastq", "-o", "fastq/%s" % snakemake.wildcards.sample, snakemake.input[0]])
 else:
-    subprocess.run(["fastq-dump", sraId, "-Z", "--gzip"] stdout=open(snakemake.output[0], "w"))
+    subprocess.run(["fastq-dump", sraId, "-Z", "--gzip"], stdout=open(snakemake.output[0], "w"))
